0fbf45ceb190b366e4d3d7836c2a9002c36c79c2
Wayne <hoangtrandaingoc@gmail.com>
2020-06-03 20:54:35 +0700

    Updates

---
+++ precache-manifest.efa01c2cd90bcf5867ea51ad64fdc933.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b26567ed76194720c58fc84be5fed657",
+    "url": "/movie-finder/index.html"
+  },
+  {
+    "revision": "896a0401977c4dd084f5",
+    "url": "/movie-finder/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "1bb18a738b95b3b12da8",
+    "url": "/movie-finder/static/css/main.392da065.chunk.css"
+  },
+  {
+    "revision": "896a0401977c4dd084f5",
+    "url": "/movie-finder/static/js/2.6d5cebb9.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/movie-finder/static/js/2.6d5cebb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bb18a738b95b3b12da8",
+    "url": "/movie-finder/static/js/main.2bae49e5.chunk.js"
+  },
+  {
+    "revision": "6e4af5923af954cae8e2",
+    "url": "/movie-finder/static/js/runtime-main.cee4ef07.js"
+  }
+]);
